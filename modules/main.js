98753c44ba83d09f5b5ecf7dac3993566a5604f3
Calvin Wei <cwei33@hotmail.com>
2018-03-22 01:46:38 +0000

    Add URL routing for /

--- modules/main.js
+++ modules/main.js
@@ -1,1 +1,8 @@
 angular.module('main', ['ngRoute'])
+	.config(['$routeProvider', function($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'templates/monte_carlo.html',
+			controller: 'simCtrl',
+			controllerAs:  'ctrl'
+		});
+	}]);
