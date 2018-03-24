05293e9e9d62ed4906f7b9e26b01024896162701
Calvin Wei <cwei33@hotmail.com>
2018-03-24 00:34:02 +0000

    Add two-way data binding of input fields

--- modules/controllers.js
+++ modules/controllers.js
@@ -1,3 +1,13 @@
 function SimCtrl() {
-	
+	this.roundsPerTrial = 1;
+	this.numTrials = 1;
+	this.betAmt = 5;
+	this.passLine = true;
+	this.betFour = 0;
+	this.betFive = 0;
+	this.betSix = 0;
+	this.betEight = 0;
+	this.betNine = 0;
+	this.betTen = 0;
+	this.disableButton = false;
 }
